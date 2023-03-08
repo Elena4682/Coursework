3733ace470710c0e96cc74c7ec8b8396a6473b1c
Елена <simonaitene@yandex.ru>
2023-03-08 22:56:30 +0300

    на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Employee[] employee = new Employee[10];
+        employee [0]= new Employee(" Иванов Иван Иванович ", 3, 40000);
+        employee [1]= new Employee(" Семенова Татьяна Андреевна ", 1, 50000);
+        employee [2]= new Employee(" Петров Степан Алексеевич" , 2, 40000);
+        employee [3]= new Employee(" Гущин Алексей Иванович" , 4, 45000);
+        employee [4]= new Employee(" Иванова Вера Григорьевна ", 5, 50000);
+        for (int i = 0; i < employee.length; i++) {
+            if (i == employee.length-1){
+                System.out.println(employee[i]);
+                break;
+            }
+            System.out.println(employee[i] + " ");
+
+        }
+
+        }
+
+
+
+        }
